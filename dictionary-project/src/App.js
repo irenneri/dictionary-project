2ec100cecbe2cdf3ef3addfbf1a9dc60ebfd66a1
Ira Bartosh <irenbarto@gmail.com>
2022-10-24 15:59:14 +0300

    Update App.js

--- dictionary-project/src/App.js
+++ dictionary-project/src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        
       </header>
     </div>
   );
