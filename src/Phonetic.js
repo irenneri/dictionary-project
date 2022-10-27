3be18cb35f1679d7c9fb084ec7d19ea35cd55ddf
Ira Bartosh <irenbarto@gmail.com>
2022-10-27 15:45:32 +0300

    Add Phonetic.js

---
+++ src/Phonetic.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Phonetic(props) {
+    console.log(props.phonetic);
+    return (
+        <div className="Phonetic">
+            <a href={props.phonetic.audio}>Listen</a>
+            <br/>
+            {props.phonetic.text}
+        </div>
+    );
+}
